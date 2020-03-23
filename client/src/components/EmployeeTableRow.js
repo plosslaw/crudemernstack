3c64406d686f9d8f80e8e4213e216d8b6d4d550a
plosslaw <spacequartz@gmail.com>
2020-03-23 11:13:21 +0800

    fixed $ for salary implementation

--- client/src/components/EmployeeTableRow.js
+++ client/src/components/EmployeeTableRow.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 const EmployeeTableRow = (props)=>{
     const {firstName, lastName, job, salary, _id} = props.employee
+    const salaryindollars = '$'+salary
     return(
         <tr>
             <th scope="row">{_id}</th>
             <td>{firstName}</td>
             <td>{lastName}</td>
             <td>{job}</td>
-            <td>{salary}</td>
+            <td>{salaryindollars}</td>
             <td>
                 <div class="btn-group" role="group" aria-label="Basic example">
                     <button type="button" onClick={props.showEditForm.bind(this, props.employee)} className="btn btn-secondary">Edit</button>
--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -5,9 +5,6 @@ const Input = (props)=>{
     if(typeof capitalized === 'string'|| capitalized instanceof String){
         const temp = capitalized.replace(/^\w/, char=>char.toUpperCase())
         capitalized = temp
-    }else if(props.name == 'salary'){  
-        const temp = '$' + capitalized
-        capitalized = temp;
     }
     return (
         <>
